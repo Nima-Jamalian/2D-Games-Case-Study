bace1a4d28d67540245e64e9a51ada1a8a11cc73
Nima Jamalian <nima.jamalian80@gmail.com>
2023-03-21 22:11:10 +0000

    Update

---
+++ Assets/Scripts/AsteroidCharachterContoller.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidCharachterContoller : MonoBehaviour
+{
+    [SerializeField] float rotation = 0;
+    [SerializeField] float speed = 3;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] Transform laserSpawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Rotation();
+        Shooting();
+    }
+
+    void Movement() {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+    }
+    
+    void Rotation() {
+        Vector3 direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+    }
+
+    void Shooting() {
+        if (Input.GetMouseButtonDown(0)) {
+            Instantiate(laserPrefab, laserSpawnPoint.position, transform.rotation);
+        }
+    }
+}
